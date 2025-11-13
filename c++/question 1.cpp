af1bdea0a23aaad1c40b94184289c5860889cadb
Nack-glitch <ngudeta87@gmail.com>
2025-11-13 02:45:27 -0800

    question 1

---
+++ c++/question 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+float dec1 , dec2 , product ;
+cout<<"enter first decimal number: ";
+cin>>dec1 ;
+cout<<"enter second decimal number: ";
+cin>>dec2 ;
+product = dec1 * dec2 ;
+cout<<"THE PRODUCT IS " <<product ;
+}
