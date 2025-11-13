eeebaaffcbd808537f3852978cbea72c7be2fa28
Nack-glitch <ngudeta87@gmail.com>
2025-11-13 03:00:50 -0800

    question 3

---
+++ c++/question 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+const double PI = 3.14 ;
+double radius, curcumfrence ;
+
+cout<<"enter the radius: ";
+cin>>radius;
+curcumfrence = 2 * PI * radius;
+
+cout<<"the curcumfrence is: "<<curcumfrence<<endl;
+}
