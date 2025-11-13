48d998a85a729f98eb0e9359fffe81707dbb1f4e
Nack-glitch <ngudeta87@gmail.com>
2025-11-13 02:53:40 -0800

    question 2

---
+++ c++/question 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int  age ;
+string name ;
+cout<<"enter your name"<<endl;
+cin>>name;
+cout<<"enter your age"<<endl;
+cin>>age;
+
+cout<<"your name is "<<name<<endl;
+cout<<"your age is "<<age<<endl;
+}
