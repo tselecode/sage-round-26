afa36e95cdaf72b3de39fbacc61d5e6918d18fae
Nack-glitch <ngudeta87@gmail.com>
2025-11-13 02:25:48 -0800

    addition project

---
+++ c++/addition project.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int num1 , num2  ;
+
+cout<<"Enter first number :";
+cin>>num1 ;
+cout<<"Enter second number :";
+cin>>num2 ;
+int sum = num1 + num2 ;
+cout<<"the sum is "<<sum<<endl;
+
+}
