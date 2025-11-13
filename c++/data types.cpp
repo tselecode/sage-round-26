678f5133d058d26679f84efa931c9bda5834e2c4
Nack-glitch <ngudeta87@gmail.com>
2025-11-13 01:24:47 -0800

    data types

---
+++ c++/data types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int age = 19;
+	float cost = 19.5;
+	double pi = 19.56453206547873d;
+	char dollar = '$';
+	bool isTrue = false;
+
+	cout<<age<<endl;
+	cout<<cost<<endl;
+	cout<<pi<<endl;
+	cout<<dollar<<endl;
+	cout<<isTrue<<endl;
+	return 0;
+}
