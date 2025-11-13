7ac9fb59035078a94cbb04fae8ada790cb010c19
Nack-glitch <ngudeta87@gmail.com>
2025-11-13 02:36:13 -0800

    celcius to F converter

---
+++ c++/celcius to f converter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+float celscius , fahraniet;
+cout<<"enter temprature: ";
+cin>>celscius ;
+ fahraniet = (celscius * 1.8) + 32;
+cout<<fahraniet;
+}
