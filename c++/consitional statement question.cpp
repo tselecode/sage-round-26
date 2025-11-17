2952aa9ee6441d3490e03eee2b5940154e3c08ca
natnael <ngudeta87@gmail.com>
2025-11-17 12:25:33 +0300

    conditional statement question

---
+++ c++/consitional statement question.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double number = 0;
+	
+	if (number>0){
+		cout<<"positive number"<<endl;
+		
+	}else if(number<0){
+		cout<<"negative number"<<endl;
+	}else{
+		cout<<"zero"<<endl;
+	}
+	}
