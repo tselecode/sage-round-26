e56a7e11c960af225857f5a0ca78f9e59c91708b
tselecode <tseloteeskiyas@gmail.com>
2025-11-18 13:47:16 +0300

    array sum(2)

---
+++ c++/array sum(2).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int num1[3] = {23 , 34 , 5};
+int num2[3] = {5 , 34 , 1};
+int num3[3] = {5 , 78 , 9};
+int sum = 0 ;
+int sum2 = 0 ;
+int sum3= 0 ;
+
+for(int i = 0 ; i<3 ; i++){
+	sum = sum + num1[i];
+}
+for(int i = 0 ; i<3 ; i++){
+	sum2 = sum + num2[i];
+}
+for(int i = 0 ; i<3 ; i++){
+	sum3 = sum + num3[i];
+}
+cout<<sum<<endl;
+cout<<sum2<<endl;
+cout<<sum3<<endl;
+
+}
