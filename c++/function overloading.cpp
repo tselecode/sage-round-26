9f3b88039113c4bfed564591b7d70987f52a983a
tselecode <tseloteeskiyas@gmail.com>
2025-11-20 11:36:52 +0300

    function overloading

---
+++ c++/function overloading.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int add(int x , int y){
+	return x+y;
+}
+int add(int x , int y , int z){
+	return x+y+z;
+}
+int add(int x , int y , int z , int a){
+	return x+y+z+a;
+}
+
+int main(){
+ 
+ 
+ cout<<add(4,4,4,6 );
+}
