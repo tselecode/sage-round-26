7aef7989d89c9770d69e039c05f4c88125800d52
tselote <tseloteeskiyas@gmail.com>
2025-12-05 11:02:54 +0300

    c++ projects

--- projects/c++ project/multiplication table.cpp
+++ projects/c++ project/multiplication table.cpp
@@ -3,21 +3,21 @@
 using namespace std;
 
 int main() {
-    cout << "Complete Multiplication Table (1-10)\n";
-    cout << "====================================\n\n";
+    cout << " Multiplication Table (1-10)\n";
+    cout << "====================================";
     
-    // Header row
+  
     cout  << "×" << " |";
     for (int i = 1; i <= 10; i++) {
         cout  << i;
     }
     cout << "\n----+";
     for (int i = 1; i <= 10; i++) {
-        cout << "----";
+        cout << "   ";
     }
     cout << endl;
     
-    // Table body
+   
     for (int i = 1; i <= 10; i++) {
         cout  << i << " |";
         for (int j = 1; j <= 10; j++) {
